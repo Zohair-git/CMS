edc5b81867049916f4573c6f26894a4edb8a7611
Huzaifa1509 <119890303+Huzaifa1509@users.noreply.github.com>
2024-01-22 20:07:51 +0500

    check
    check db and theme

---
+++ CMS/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using CMS.Data;
+using CMS.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CMS.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IHttpContextAccessor cont;
+        private readonly db_cmsContext db;
+
+
+        public HomeController(ILogger<HomeController> logger , db_cmsContext db , IHttpContextAccessor cont)
+        {
+            _logger = logger;
+            this.db = db;
+            this.cont = cont;
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CMS/Models/TblFeedback.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CMS.Models
+{
+    public partial class TblFeedback
+    {
+        public int Id { get; set; }
+        public int? UserId { get; set; }
+        public string? Text { get; set; }
+        public int? PId { get; set; }
+
+        public virtual TblClientRegister IdNavigation { get; set; } = null!;
+        public virtual TblProduct? PIdNavigation { get; set; }
+    }
+}
