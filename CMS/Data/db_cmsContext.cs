d72258e7109fdcb87c575bb34c7ab0b94545d8c5
Zohair Adeel <zohairadeel987@gmail.com>
2024-01-27 18:17:50 +0500

    latest

--- CMS/Data/db_cmsContext.cs
+++ CMS/Data/db_cmsContext.cs
@@ -33,7 +33,7 @@ namespace CMS.Data
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=.;Initial Catalog=db_cms;Persist Security Info=False;User ID=sa;Password=aptech;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;");
+                optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=db_cms;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
             }
         }
 
